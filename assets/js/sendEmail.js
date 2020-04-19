5f49e78f058c7e2edb5873ea75921502f5b6fa28
Fatima Aminu <teemamin@yahoo.com>
2020-04-19 14:22:31 +0000

    email implementation added to contact.html

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,20 @@
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;  // To block from loading a new page
+}
+
+//here i am using contactform as placeholder/var for the html form in my contact.html
+//note that the keys for the object is from emailjs template we created, back their site.
